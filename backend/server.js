42d96cb960f299dd2bbb594616740d2541ecd9a8
jawad-r <jawadaliraja2022@gmail.com>
2026-02-12 17:29:25 +0500

    redirecting to url

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'https://job-posting-ashy-eight.vercel.app/'));
+  res.redirect('https://job-posting-ashy-eight.vercel.app/');
 });
 
 const PORT = process.env.PORT || 5001;
